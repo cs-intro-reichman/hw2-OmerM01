8e5ec211c190071b0b54b456089dffe832e35194
OmerM01 <omerwork00@gmail.com>
2024-11-13 20:18:35 +0200

    HW02

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,24 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0];
+                word = word.toUpperCase();
+                int num = Integer.parseInt(args[1]);
+                String an = " a";
+                String specialChars = "AEFHILMNORSX";
+                for(int i = 0; i < word.length(); i++){
+                       for(int j = 0; j < specialChars.length(); j++){
+                                if(specialChars.charAt(j) == word.charAt(i)){
+                                        an = "an";
+                                }
+                       }
+                        System.out.println("Give me " + an + "  " + word.charAt(i) + "!");
+                        an = " a";
+                }
+                System.out.println("What does that spell?");
+                for(int i = 0; i < num; i++){
+                        System.out.println(word + "!!!");
+                }
+
         }
 }
