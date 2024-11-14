113ff7fd4eca2b9f7312096347a231c16ab96cf8
OmerM01 <omerwork00@gmail.com>
2024-11-14 08:13:04 +0200

    newTry

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int num = Integer.parseInt(args[0]);
+		double sum = 1;
+		double uneven = 3;
+		for(int i = 1; i <= num; i++){
+			if(i % 2 != 0){
+				uneven = uneven * (-1);
+				System.out.println(uneven);
+			}
+			sum = sum + (1/uneven);
+			System.out.println(sum);
+			uneven = Math.abs(uneven) + 2;
+			System.out.println(uneven);
+		}
+		System.out.println(sum * 4);
 	}
 }
