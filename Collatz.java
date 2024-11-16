498f0bbafeb33df33f3b5c9445d5c2df66878a30
OmerM01 <omerwork00@gmail.com>
2024-11-16 22:23:05 +0200

    jjj

--- Collatz.java
+++ Collatz.java
@@ -1,27 +1,36 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // int seed = Integer.parseInt(args[0]);
-		// //String mode = args[1];
-		// //int calc = seed;
-		// for(int i = 2; i <= seed; i++){
-		// 	int sum = 0;
-		// 	int calc = i;
-		// 	System.out.print(calc);
-		// 	while(calc != 1){
-		// 		if(calc % 2 == 0){
-		// 			calc = calc/2;
-		// 		}
-		// 		else{
-		// 			calc = (calc * 3) + 1;
-		// 		}
-		// 		System.out.print("" + calc );
-		// 		sum ++;
-		// 	}
-		// 	System.out.println("(" + sum + ")");
-		// }
-		
-		// System.out.println(seed);
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+        int sum;
+
+		for(int i = 1; i <= seed; i++){
+			int pointer = i;
+			sum = 0;
+
+			if(mode.equals("V")){ //prints the num from where it begins
+				System.out.print(pointer);
+			}
+
+			while(pointer != 1){
+				if(pointer % 2 == 0){
+					pointer = pointer/2;
+				} else{
+					pointer = (pointer * 3) + 1;
+				}
+
+				if(mode.equals("V")){
+					System.out.print(" " + pointer );
+				}
+				sum++;
+			}
+
+			if (mode.equals("v")) {
+                System.out.println(" (" + sum + ")");
+            }
+		}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 
 	}
 }
